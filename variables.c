ab6cb495caaeba13dff24634092cbef9077d7f04
meenakshibathuluri <meenakshibathuluri@gmail.com>
2025-10-19 21:19:23 +0530

    variables program uploaded

---
+++ variables.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdbool.h>
+void main() {
+  // int a = 30;
+   //int b = 50;
+   //int sum = a+b;
+   //printf("you are %d\n",a);
+   //printf("you are %d\n",b); 
+   //printf("sum is %d",sum);  
+   
+   //float num1 = 19.45;
+   //float num2 = 100.54;
+   //printf("yuor num1 %.2f\n",num1);
+   //printf("your num2 %.1f\n",num2);
+  // double num1 = 30.123456812345678;
+   //double num2 = 100.1234567878452;
+   //printf("the value of num1 is %.15lf\n",num1);
+   //printf("the vlaue of num2 %lf\n",num2);
+   //char grade = 'b';
+   //char section = 'f';
+   //char symbol = '*';
+   //printf("yuor grade is %c\n",grade);
+   //printf("yuor section is %c\n",section);
+   //printf("symbol is %c\n",symbol);
+   // char name[] = "meenakshi";
+   // char course[] = "c program";
+    //printf("hello %s\n",name);
+    //printf("welcome to %s\n",course);
+    bool isOnline = true;
+    if(isOnline) {
+       printf("you are isOnline");
+    }else{
+       printf("you are isOffline");
+    }
+}
