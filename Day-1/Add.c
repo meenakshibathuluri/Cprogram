350916cb624385a6713165b6fbf96f34e7fb4098
meenakshibathuluri <meenakshibathuluri@gmail.com>
2025-10-30 18:54:30 +0530

    c program uploaded

---
+++ Day-1/Add.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    int num1=10,num2=20,sum;
+    printf("enter two value: ");
+    scanf("%d %d",&num1,&num2);
+    sum = num1 + num2;
+    printf("sum of two number is %d",sum);
+    return 0;
+}
---
+++ Day-2/Fromat.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    int num1=20,num2=10,sub;
+    printf("enter two value: ");
+    scanf("%d %d",&num1,&num2);
+    sub = num1 - num2;
+    printf("sub of two number is %d",sub);
+    return 0;
+}
