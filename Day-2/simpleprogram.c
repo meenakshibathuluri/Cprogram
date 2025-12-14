a2357d48ead8a87ef0a22818e82738cb5b8a5741
meenakshibathuluri <meenakshibathuluri@gmail.com>
2025-12-14 13:12:48 +0530

    c program uploaded

---
+++ Day-2/simpleprogram.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+     printf("Hello world");
+     int a = 10;
+     int b = 40;
+     char gender = 'f';
+     printf("\n the integer value frist is: %d, second value is: %d, gender is: %c",a,b,gender);
+     float f = 10.7;
+     printf("\n%f",f);
+     return 0;
+}
