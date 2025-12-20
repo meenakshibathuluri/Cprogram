71a5121d376b8d99176fbf22c571ec966af754d8
meenakshibathuluri <meenakshibathuluri@gmail.com>
2025-12-20 18:38:11 +0530

    cprogram uploaded

--- Day-4/perimeterofrectangle.c
+++ Day-4/perimeterofrectangle.c
@@ -8,3 +8,9 @@ int main() {
      return 0;
 }
 
+//int a,perimeter,b;
+  //   printf("enter the value of a and b:");
+//     scanf("%d %d", &a,&b);
+//     perimeter = 2*a+2*b;
+//     printf("perimeter of a rectangle is %d", perimeter);
+//   return 0;
