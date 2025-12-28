57710b38afb91288609f08983a75dc66fe0c5a5d
meenakshibathuluri <meenakshibathuluri@gmail.com>
2025-12-28 19:17:49 +0530

    cprogram uploaded

---
+++ Day-6/bitwise.c
@@ -0,0 +1,40 @@
+// bitwise and 
+#include<stdio.h>
+int main() {
+     int a,b;
+     printf("enter the value of a and b");
+     scanf("%d %d",&a,&b);
+      printf("%d", (a&b));
+     return 0;
+}
+
+// bitwise or
+#include<stdio.h>
+int main() {
+     int a,b;
+     printf("enter the value of a and b");
+     scanf("%d %d",&a,&b);
+      printf("%d", (a|b));
+     return 0;
+}
+
+
+// bitwise XOR
+#include<stdio.h>
+int main() {
+     int a,b;
+     printf("enter the value of a and b");
+     scanf("%d %d",&a,&b);
+      printf("%d", (a^b));
+     return 0;
+}
+
+// bitwise compliment
+#include<stdio.h>
+int main() {
+     int a,b;
+     printf("enter the value of a and b");
+     scanf("%d %d",&a,&b);
+      printf("%d", (a&b));
+     return 0;
+} 
