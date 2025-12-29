a5443793ecd632e167a3dd4a6810a616feaede31
meenakshibathuluri <meenakshibathuluri@gmail.com>
2025-12-29 18:50:13 +0530

    cprogram uploaded

---
+++ Day-7/positiveornegative.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    int n;
+    printf("enter the value of n:");
+    scanf("%d",&n);
+    if(n>=0) 
+    printf("it is positive");
+    else
+    printf("it is a negative");
+}
